f1df3c87e1dbfef9fb753365562db117f7d8ed05
Justin Sohn <jusohn2015@gmail.com>
2021-08-16 11:28:28 -0400

    commit tree 8b315f00bba23b136d3e5d215e557a0867ff4685

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const remove = document.getElementById('main');
+remove.parentNode.removeChild(remove);
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('Id', 'victory');
+
